d4556db9fea296f76e366669c8af84e7b8c1b9c8
Boddenberg <filipeboddenberg@yahoo.com.br>
2026-02-23 22:07:18 -0300

    first commit

---
+++ src/main/kotlin/com/vidasync_bff/VidasyncBffApplication.kt
@@ -0,0 +1,11 @@
+package com.vidasync_bff
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VidasyncBffApplication
+
+fun main(args: Array<String>) {
+	runApplication<VidasyncBffApplication>(*args)
+}
---
+++ src/test/kotlin/com/vidasync_bff/VidasyncBffApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vidasync_bff
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VidasyncBffApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
